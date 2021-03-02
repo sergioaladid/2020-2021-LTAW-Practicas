408b9cd881bf2b0a0dc2b709c4ac09b48a02eba9
sergioaladid <sergioaladid@hotmail.com>
2021-03-02 14:50:28 +0100

    Mira la extension del recurso

--- P1/tienda.js
+++ P1/tienda.js
@@ -5,14 +5,41 @@ const PUERTO = 9000;
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
 
-    const data = "main.html"
+    let fichero = ""
+    const url = new URL(req.url, 'http://' + req.headers['host']);
 
-    fs.readFile(data, null, function (error, data) {
+    console.log("Recurso solicitado (URL):" + req.url)
+    console.log("Recurso:" + url.pathname)
+
+    if (url.pathname == "/") //--http://localhost:8080/
+        fichero = "main.html"  //--Página principal
+        
+    else{
+        fichero = url.pathname; //-- q.pathname es otro recurso que se pide en el localhost
+    }
+    //-- Para sacar el tipo de fichero
+    extension = fichero.split(".")[1] //--Se coge la extensión del archivo
+    //fichero = "." + fichero //--Para leer el fichero. Sin "." no funciona
+    console.log("Final:" + fichero)
+    console.log("Extension Final:" + extension)
+    
+    fs.readFile(fichero, null, function (error, data) {
         if (error) {
             res.writeHead(404);
             res.write('Whoops! File not found!');
         } else {
-            res.setHeader("Content-Type", "text/html")
+            if (extension == "jpg" || extension == "png"){
+                tipo_texto = "image/" + extension;
+            }
+
+            if (extension == "css"){
+                tipo_texto = "text/css";
+            }
+
+            if (extension == "html"){
+                tipo_texto = "text/html";
+            }
+            res.setHeader("Content-Type", tipo_texto)
             res.write(data);
         }
         res.end();
